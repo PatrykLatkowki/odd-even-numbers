d768656f54f6e39f9d59da801f799e7123ac9960
Patryk Łatkowski <patlat.91@gmail.com>
2021-07-29 18:58:39 +0200

    Add GetInput method with validation and display if number is odd or even.

--- OddEvenNumbers/Program.cs
+++ OddEvenNumbers/Program.cs
@@ -10,6 +10,22 @@ namespace OddEvenNumbers
     {
         static void Main(string[] args)
         {
+            var number = GetInput();
+
+            Console.WriteLine(number % 2 == 0 ? "Liczba parzysta" : "Liczba nieparzysta");
+        }
+
+        private static int GetInput()
+        {
+            while (true)
+            {
+                Console.Write("Podaj dowolną liczbę całkowitą: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                    Console.WriteLine("Podana liczba nie jest liczbą całkowitą!");
+                else
+                    return number;
+            }
         }
     }
 }
